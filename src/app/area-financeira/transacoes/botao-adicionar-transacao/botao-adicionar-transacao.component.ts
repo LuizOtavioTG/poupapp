3edc5a7b2bc30c65f1e9a699421b74c181c6aebe
LuizOtavioTG <bc.luizo@gmail.com>
2025-06-25 16:26:07 -0300

    feat: adicionando link de campos do formulário com o component

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,18 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
+import { FormsModule } from "@angular/forms"
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
 export class BotaoAdicionarTransacaoComponent {
+
   modalAberto = signal(false);
 
-  abrirModal() {
-   this.modalAberto.set(true);
+  novaTransacaoForm = {
+    nome: '',
+    tipo: '',
+    valor: '',
+    data: '',
+    conta: ''
   }
-   
+
+  abrirModal() {
+    this.modalAberto.set(true);
+  }
+
+  aoSubmeter() {
+    console.log(this.novaTransacaoForm)
+  }
+
 }
